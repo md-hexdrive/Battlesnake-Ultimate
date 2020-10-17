69dffea039502c221893d0c2f97891345a38a952
Ronan156 <cedarstream165@gmail.com>
2020-10-17 15:06:47 +0000

    Add food avoidance

--- behaviour.py
+++ behaviour.py
@@ -48,6 +48,9 @@ def snake_behaviour(data):
     # look for food, if I should do that now
     if hunt_food and len(possible_moves) > 0 and eat_food(board, possible_moves):
         move = goto.find_food(board, curr_pos, possible_moves)
+    else:
+        possible_moves = avoid_food(board, possible_moves)
+    
     # pick a random safe move
     if len(possible_moves) > 0 and move == None:
         move = moves.pick_move(possible_moves)
@@ -82,4 +85,18 @@ def search_for_moves(board, curr_pos, ignored=[]):
 
 # Should I eat Food?
 def eat_food(board, possible_moves):
-    return True
+    #snakes =
+    if board.me.health < 99:
+        return True
+    else:
+        return False
+
+def avoid_food(board, possible_moves):
+    returned_moves = dict()
+    if not eat_food(board, possible_moves):
+        for name, move in possible_moves.items():
+            if not board.is_food(move):
+                returned_moves[name] = move
+    if len(returned_moves) > 0:
+        return returned_moves
+    return possible_moves
